ad2e4251a330da7b752fbc4ffd8c4b01c02cb186
pablitog21 <juandavid.pablitogame@gmail.com>
2024-03-07 17:12:43 -0500

    decline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@
 import BotonWhatsapp from "@/components/BotonWhatsapp ";
 import Image from "next/image";
 
+
+import { useState } from 'react';
+
 export default function Home() {
+  const [hovered, setHovered] = useState(false);
 
   const handleDownload = () => {
     const downloadUrl = '/assets/doc/cvc.doc';
@@ -12,116 +16,102 @@ export default function Home() {
     link.click();
   };
 
+  const handleHover = () => {
+    setHovered(true);
+  };
+
+  const handleLeave = () => {
+    setHovered(false);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+    <main className="flex flex-col min-h-screen p-8 lg:p-24">
+      <div className="max-w-5xl mx-auto mb-8 lg:flex lg:justify-between lg:items-center">
+        <p className="fixed top-0 left-0 w-full py-8 text-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-lg dark:border-neutral-800 dark:bg-zinc-800/30 lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Mi Portafolio
         </p>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+      <div className="relative flex justify-center mb-8 lg:mb-0">
+        <img
           src="/assets/icons/four-icon.png"
           alt="One Icon"
-          width={350}
-          height={350}
-          priority
+          className={`w-full h-full max-w-screen-sm ${
+            hovered ? 'lg:max-w-lg' : 'lg:max-w-xl'
+          } transition-all duration-500 ease-in-out ${
+            hovered ? 'scale-110' : 'scale-100'
+          }`}
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
         />
       </div>
 
-      <div className="mb-8 text-center hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 rounded-lg p-4 lg:max-w-3xl lg:w-full lg:text-left lg:mx-auto">
-        <p className="text-lg font-semibold mb-2 text-center">¡Hola! ¡Soy Juan David Marin Zuluaica!</p>
-        <p className="text-sm text-gray-500 text-center leading-relaxed">
-          Me encanta trabajar en proyectos emocionantes y aprender nuevas tecnologías.
-          En mi tiempo libre, disfruto explorar las últimas tendencias en desarrollo de software y participar en comunidades de programación.
-          También me apasiona compartir conocimientos y ayudar a otros desarrolladores a crecer en sus habilidades.
-          Siempre estoy buscando nuevas oportunidades para colaborar en proyectos innovadores y desafiantes.
+      <div className="text-center lg:text-left">
+        <p className="text-lg font-semibold mb-4">¡Hola! ¡Soy Juan David Marin Zuluaica!</p>
+        <p className="text-sm text-gray-500 leading-relaxed">
+          Me encanta trabajar en proyectos emocionantes y aprender nuevas tecnologías. En mi tiempo libre, disfruto explorar las últimas tendencias en desarrollo de software y participar en comunidades de programación. También me apasiona compartir conocimientos y ayudar a otros desarrolladores a crecer en sus habilidades. Siempre estoy buscando nuevas oportunidades para colaborar en proyectos innovadores y desafiantes.
         </p>
       </div>
 
-      <div className="mb-8 text-center">
+      <div className="text-center mb-8">
         <button
           onClick={handleDownload}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
-          CVC
+          Descargar CVC
         </button>
       </div>
-      
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
+      <div className="grid grid-cols-1 gap-8 text-center lg:grid-cols-3 lg:text-left">
         <a
           href="https://github.com/pablitog21"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group block px-5 py-4 border rounded-lg transition-colors hover:bg-gray-100 hover:border-gray-300 hover:dark:bg-neutral-800/30 hover:dark:border-neutral-700"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            GitHub {" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Mi perfil de GitHub
-          </p>
+          <h2 className="text-2xl font-semibold mb-2">GitHub</h2>
+          <p className="text-sm opacity-50">Mi perfil de GitHub</p>
         </a>
 
         <a
           href="https://www.instagram.com/pablitog_21"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group block px-5 py-4 border rounded-lg transition-colors hover:bg-gray-100 hover:border-gray-300 hover:dark:bg-neutral-800/30 hover:dark:border-neutral-700"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Instagram{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Mi perfil de Instagram
-          </p>
+          <h2 className="text-2xl font-semibold mb-2">Instagram</h2>
+          <p className="text-sm opacity-50">Mi perfil de Instagram</p>
         </a>
-
- z
 
         <a
           href="https://www.linkedin.com/in/juan-david-mar%C3%ADn-zuluaica-5339a820b"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group block px-5 py-4 border rounded-lg transition-colors hover:bg-gray-100 hover:border-gray-300 hover:dark:bg-neutral-800/30 hover:dark:border-neutral-700"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Linkedin{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Mi perfil de Linkedin
-          </p>
+          <h2 className="text-2xl font-semibold mb-2">LinkedIn</h2>
+          <p className="text-sm opacity-50">Mi perfil de LinkedIn</p>
         </a>
-
       </div>
 
-      {/* <div className="mb-8 lg:max-w-3xl lg:w-full lg:mx-auto">
-        <div className="p-10 rounded-lg overflow-hidden">
-          <iframe
-            style={{ borderRadius: "0.5rem" }}
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/Bznxx12Ptl0?autoplay=1&mute=1"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+        <div className="text-center bg-gray-50 dark:bg-gray-800 p-6 rounded-lg w-full max-w-[90vw] sm:max-w-md mx-auto">
+          <img
+            src="/assets/icons/two-icon.png"
+            alt="Descripción de la imagen"
+            className="w-20 h-20 sm:w-auto sm:h-auto"
+          />
+          <p className="mt-4 text-base dark:text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, cum dolorum impedit doloremque libero repellendus ad rem excepturi quae fugiat ea nihil atque accusantium placeat et doloribus in! Dolorem, voluptates.</p>
         </div>
-      </div> */}
-      <BotonWhatsapp/>
+        <div className="text-center bg-gray-50 dark:bg-gray-800 p-6 rounded-lg w-full max-w-[90vw] sm:max-w-md mx-auto">
+          <img
+            src="/assets/icons/two-icon.png"
+            alt="Descripción de la imagen"
+            className="w-20 h-20 sm:w-auto sm:h-auto"
+          />
+          <p className="mt-4 text-base dark:text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, cum dolorum impedit doloremque libero repellendus ad rem excepturi quae fugiat ea nihil atque accusantium placeat et doloribus in! Dolorem, voluptates.</p>
+        </div>
+      </div>
     </main>
   );
 }
